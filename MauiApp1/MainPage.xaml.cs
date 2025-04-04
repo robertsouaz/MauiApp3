d6ed1d342ba43fe99215694f9ee98798c9fe203b
etechas <etechas@LI08-09>
2025-04-04 20:36:06 -0300

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        private void FlipButton_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+}
