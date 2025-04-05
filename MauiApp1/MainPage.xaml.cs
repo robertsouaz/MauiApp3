8e4777c04a172e95c1675f52869840d89bfa0e2b
etechas <etechas@LI08-09>
2025-04-04 22:11:43 -0300

    22:00

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,11 +1,32 @@
-﻿namespace MauiApp1
+﻿using System;
+namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
+        
         private void FlipButton_Clicked(object sender, EventArgs e)
         {
-            
+            Random rnd = new Random();
+            int sorteio = rnd.Next(2);
+
+            if (selecaoPiker.SelectedIndex == 0 && sorteio == 0)
+            {
+                DisplayAlert("Você ganhou!", "Você escolheu cara e saiu cara!", "OK");
+            }
+            else
+            {
+                if (selecaoPiker.SelectedIndex == 1 && sorteio == 1)
+                {
+                    DisplayAlert("Você ganhou!", "Você escolheu coroa e saiu coroa!", "OK");
+                }
+               
+            }
+
+
+
         }
+
+
     }
 
 }
